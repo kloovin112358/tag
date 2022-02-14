021054fa3d5f3bfc4fa92664c436b07c9170e805
Kevin Lauer <kevin.lauer2019@gmail.com>
2022-02-13 20:08:00 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
+import React, {useEffect, useState, useRef} from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { gsap } from "gsap";
 
 function App() {
+
+  const cardRef = useRef();
+
+  useEffect(() => {
+    gsap.to(cardRef.current, { rotation: "+=360" });
+  });
+
   return (
     <>
-      <Navbar bg="light" className="border-bottom">
+      <Navbar bg="danger" variant="dark">
         <Container>
-          <Navbar.Brand href="/">T.A.G.</Navbar.Brand>
+          <Navbar.Brand href="/" className="fw-bold text-light">T.A.G.</Navbar.Brand>
         </Container>
       </Navbar>
       <Container>
         <div className="pt-3 pb-5">
           <Row>
-            <Col>
+            <Col lg={10}>
               <Card>
+                <Card.Body>
+                  <Card.Text>
+                  What does HTML stand for?
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="mt-3 mt-lg-0">
+              <Card ref={cardRef}>
                 <ListGroup variant="flush">
                   <ListGroup.Item>Player 1 - 100 pts</ListGroup.Item>
                   <ListGroup.Item>Player 2 - 200 pts</ListGroup.Item>
                   <ListGroup.Item>Player 3 - 300 pts</ListGroup.Item>
                 </ListGroup>
-              </Card>
-            </Col>
-            <Col lg={10} className="mt-3 mt-lg-0">
-              <Card>
-                <Card.Body>
-                  <Card.Text>
-                    <p className='display-1'>What does HTML stand for?</p>
-                  </Card.Text>
-                </Card.Body>
               </Card>
             </Col>
 
